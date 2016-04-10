a48427fc811258a7f78460b448e26c3dc54c18b9
Paine Leffler <paineleffler@users.noreply.github.com>
2016-04-09 23:49:57 -0400

    makes ajax request when ingredients clicked
    short vibration if successful, long if failed, or you can go read the logs

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
 
 var UI = require('ui');
 var Vibe = require('ui/vibe');
-var Accel = require('ui/accel');
+var Ajax = require('ajax');
 
 var message;
 
@@ -14,9 +14,9 @@ var defaultCard = new UI.Card({
   title:'Load a Recipe!',
   subtitle:'Use RecipEZ App to push over recipe instructions.'
 });
-
 defaultCard.show();
 
+//menu
 function recipeIngredMenu(message){
   var menu = new UI.Menu({
       sections: [{
@@ -30,23 +30,45 @@ function recipeIngredMenu(message){
       }]
     });
     menu.on('select', function(e){
-      //
-      if(e.itemIndex === 0) 
-        Vibe.vibrate('short');
-      else 
-        Vibe.vibrate('long');
+      //recipe
+      if(e.itemIndex === 0){
+      }     
+      //ingredients
+      else {
+        request();
+      }
+
       console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
       console.log('The item is titled "' + e.item.title + '"');
     });
     menu.show();
 }
 
+//wolfram request
+function request(){
+  Ajax(
+  {
+    url: 'http://recipezwolfram-devskwod.rhcloud.com/wolframalpha?q=3.5%20ounces%20chicken&appid=9Q4Y3E-QJJR6RPR3U',
+    type: 'json'
+  },
+  function(data, status, request){
+    console.log('Success'); 
+    Vibe.vibrate('short');
+  },
+  function(error, status, request){
+    console.log('Failed', error);
+    Vibe.vibrate('long');
+  }
+  );
+}
+
+//load the recipe/ingredients menu
 defaultCard.on('click', 'up', function(e){
   recipeIngredMenu(message);
 });
 
+//event listeners
 Pebble.addEventListener('ready', function() {
-  // PebbleKit JS is ready!
   console.log('PebbleKit JS ready!');
 });
 
@@ -56,7 +78,5 @@ Pebble.addEventListener('appmessage', function(e) {
   var dict = e.payload;
   console.log('Got message: ' + JSON.stringify(dict));
   defaultCard.title = 'Recipe Loaded';
-  
-  message = e.payload;
-  
+  message = e.payload; 
 });
